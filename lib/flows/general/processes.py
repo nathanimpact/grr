7d5df5777561a152821ff6b59374d0883a336afd
Nathan Fisher <nathanimpact@users.noreply.github.com>
2015-12-15 13:09:49 +1100

    Update processes.py

--- lib/flows/general/processes.py
+++ lib/flows/general/processes.py
@@ -93,3 +93,86 @@ class ListProcesses(flow.GRRFlow):
         self.Notify("ViewObject", self.runner.output.urn,
                     "ListProcesses completed. "
                     "Listed {0:d} processes".format(num_items))
+
+
+class EnumerateProcessModules(flow.GRRFlow):
+  """List modules of running processes on a system."""
+
+  category = "/Processes/"
+  behaviours = flow.GRRFlow.behaviours + "BASIC"
+  args_type = ListProcessesArgs
+
+  @flow.StateHandler(next_state=["IterateProcesses"])
+  def Start(self):
+    """Start processing."""
+    self.CallClient("EnumerateProcessModules", next_state="IterateProcessModules")
+
+  @flow.StateHandler(next_state="HandleDownloadedFiles")
+  def IterateProcessesModules(self, responses):
+    """This stores the processes."""
+
+    if not responses.success:
+      # Check for error, but continue. Errors are common on client.
+      raise flow.FlowError("Error during process listing %s" % responses.status)
+
+    if self.args.fetch_binaries:
+      # Filter out processes entries without "exe" attribute and
+      # deduplicate the list.
+      paths_to_fetch = set()
+      for p in responses:
+        if p.exe and self.args.filename_regex.Match(p.exe):
+          paths_to_fetch.add(p.exe)
+      paths_to_fetch = sorted(paths_to_fetch)
+
+      self.Log("Got %d processes, fetching binaries for %d...", len(responses),
+               len(paths_to_fetch))
+
+      self.CallFlow(
+          "FileFinder",
+          paths=paths_to_fetch,
+          action=file_finder.FileFinderAction(
+              action_type=file_finder.FileFinderAction.Action.DOWNLOAD),
+          next_state="HandleDownloadedFiles")
+
+    else:
+      # Only send the list of processes if we don't fetch the binaries
+      skipped = 0
+      for p in responses:
+        # If we have a regex filter, apply it to the .exe attribute (set for OS
+        # X and linux too)
+        if self.args.filename_regex:
+          if p.exe:
+            if self.args.filename_regex.Match(p.exe):
+              self.SendReply(p)
+          else:
+            skipped += 1
+        else:
+          self.SendReply(p)
+      if skipped:
+        self.Log("Skipped %s entries, missing path for regex" % skipped)
+
+  @flow.StateHandler()
+  def HandleDownloadedFiles(self, responses):
+    """Handle success/failure of the FileFinder flow."""
+    if responses.success:
+      for response in responses:
+        self.Log("Downloaded %s", response.stat_entry.pathspec)
+        self.SendReply(response.stat_entry)
+
+    else:
+      self.Log("Download of file %s failed %s",
+               responses.request_data["path"], responses.status)
+
+  @flow.StateHandler()
+  def End(self):
+    """Save the results collection and update the notification line."""
+    if self.runner.output is not None:
+      num_items = len(self.runner.output)
+      if self.args.fetch_binaries:
+        self.Notify("ViewObject", self.runner.output.urn,
+                    "ListProcesses completed. "
+                    "Fetched {0:d} files".format(num_items))
+      else:
+        self.Notify("ViewObject", self.runner.output.urn,
+                    "ListProcesses completed. "
+                    "Listed {0:d} processes".format(num_items))
